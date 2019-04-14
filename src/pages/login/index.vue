9f9097e49f57227e939d3f34f46360aa3e5a8504
bei <Baer_yang@163.com>
2019-04-14 12:11:24 +0800

    完成登录页面

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,21 +1,20 @@
 <template>
-    <div class="login-container">
-        <el-form ref="form" :model="form" label-width="80px">
-  <el-form-item label="用户名" size="mini">
+    <div class="login-container" >
+        <h1 class="Login">登录</h1>
+        <el-form ref="form" :model="form" label-width="80px" >
+  <el-form-item label="用户名:">
     <el-input v-model="form.name"></el-input>
   </el-form-item>
-  <el-form-item label="密码">
-    <el-input v-model="form.name"></el-input>
+  <el-form-item label="密码:">
+    <el-input v-model="form.password" type="password"></el-input>
   </el-form-item>
 
   <el-form-item>
-    <el-button type="primary" @click="onSubmit">立即创建</el-button>
-    <el-button>取消</el-button>
+    <el-button type="primary" @click="onSubmit">登录</el-button>
   </el-form-item>
 </el-form>
     </div>
 </template>
-
 
 <script>
   export default {
@@ -41,8 +40,21 @@
   }
 </script>
 
-<style lang="less" >
- .login-container{
-     width:30% ;
- }
+<style  lang="less" >
+    .login-container{
+         width: 500px;
+         margin: 150px auto;
+          box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+          padding: 50px;
+         .el-button{
+             width: 100%;
+             
+         }
+         .Login{
+             text-align: center;
+             font-weight: 700;
+             font-size: 24px;
+             margin:0px 0 20px;
+         }
+    }
 </style>
